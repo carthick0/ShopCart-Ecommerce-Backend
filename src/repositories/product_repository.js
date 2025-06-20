3e9bd2250d4d0564eb7636976e535e919ee6099c
karthik <karthikvempala31@gmail.com>
2025-06-20 19:01:17 +0530

    Added search feature

--- src/repositories/product_repository.js
+++ src/repositories/product_repository.js
@@ -1,14 +1,44 @@
 const { where } = require('sequelize');
 const { Product } = require('../models/index');
-
+const { Op } = require('sequelize');
+const { response } = require('express');
 class ProductRepository {
-    async getProducts(limit, offset) {
+    async getProducts(limit, offset, min_price, max_price, search) {
+        const options = {
+            where: {}
+        };
+
+        if (!isNaN(limit)) {
+            options.limit = limit;
+        }
+
+        if (!isNaN(offset)) {
+            options.offset = offset;
+        }
+
+        if (min_price || max_price) {
+            const min = min_price || Number.MIN_SAFE_INTEGER;
+            const max = max_price || Number.MAX_SAFE_INTEGER;
+
+            options.where.price = {
+                [Op.between]: [min, max]
+            };
+        }
+
+        if (search) {
+            options.where.title = {
+                [Op.like]: `%${search}%`
+            };
+        }
+
         try {
-            return await Product.findAll({ limit, offset });
+            return await Product.findAll(options);
         } catch (error) {
             console.log(error);
         }
     }
+
+
 
     async getProduct(id) {
         try {
@@ -45,6 +75,21 @@ class ProductRepository {
             return response;
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    async searchProduct(searchQuery) {
+        try {
+            const response = await Product.findAll({
+                where: {
+                    title: {
+                        [Op.like]: `%${searchQuery}%`
+                    }
+                }
+            });
+            return response
+        } catch (error) {
+
         }
     }
 }
--- src/services/product_service.js
+++ src/services/product_service.js
@@ -13,9 +13,19 @@ class ProductService {
 
 
     async getProducts(query) {
-        const response = await this.repository.getProducts(+query.limit, +query.offset);
-        return response;
+        try {
+            const limit = +query.limit;
+            const offset = +query.offset;
+            const min_price = +query.min_price;
+            const max_price = +query.max_price;
+            const search = query.search
+            const response = await this.repository.getProducts(limit, offset, min_price, max_price, search);
+            return response;
+        } catch (error) {
+            console.log("ProductService getProducts error:", error);
+        }
     }
+
 
     async getProduct(id) {
         const response = await this.repository.getProduct(id);
